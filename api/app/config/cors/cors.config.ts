81fabff12c5d32b23d72ed282a77dea3cfe771b9
Yuri O. <yuri.dev44@outlook.com>
2026-08-07 09:41:00 -0400

    fix(format): run 'check:write' script to satisfy ci lint checks

--- api/app/config/cors/cors.config.ts
+++ api/app/config/cors/cors.config.ts
@@ -5,5 +5,5 @@ const FRONTEND_BASE_URL = "FRONTEND_BASE_URL"
 export const corsConfig = cors({
     origin: `${String(process.env[FRONTEND_BASE_URL])}`,
     methods: ["POST", "GET", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization"],
 })
