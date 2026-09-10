bdaf42bcf81d561d8bc4ffbc43bd933526f6041e
yuriongit <yuri.dev44@outlook.com>
2026-09-10 09:08:04 -0400

    refactor(api-cache): simplify UrlCache.get return

--- packages/api/src/features/urls/url.cache.ts
+++ packages/api/src/features/urls/url.cache.ts
@@ -2,8 +2,7 @@ import { redisClient } from "@libs/clients/storage/redis.client"
 
 export const UrlCache = {
   async get(key: string): Promise<string | null> {
-    const data = await redisClient.get(key)
-    return data ?? null
+    return await redisClient.get(key) ?? null
   },
   async set(key: string, value: string, ttlInSeconds: number): Promise<void> {
     await redisClient.set(key, value, {
