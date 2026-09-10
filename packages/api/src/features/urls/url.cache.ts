c087fc71e4c4e218b16c00828b18626368bc7940
yuriongit <yuri.dev44@outlook.com>
2026-09-10 09:12:49 -0400

    chore: run Biome check:w

--- packages/api/src/features/urls/url.cache.ts
+++ packages/api/src/features/urls/url.cache.ts
@@ -2,7 +2,7 @@ import { redisClient } from "@libs/clients/storage/redis.client"
 
 export const UrlCache = {
   async get(key: string): Promise<string | null> {
-    return await redisClient.get(key) ?? null
+    return (await redisClient.get(key)) ?? null
   },
   async set(key: string, value: string, ttlInSeconds: number): Promise<void> {
     await redisClient.set(key, value, {
