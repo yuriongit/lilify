424c67f44aa7ec7505c5c0c6aed3addb87029751
Yuri Okeren <yuri.dev44@outlook.com>
2026-09-10 09:20:31 -0400

    fix(api): correct alias resolution in deduplication functionality (#39)
    * refactor(api-cache): simplify UrlCache.get return
    
    * refactor: remove pointless comment
    
    * fix(docs): add missing FRONTEND_URL variable
    
    * fix(api): correct UrlRepo.queryAlias returns, prevents setting malformed caches and API responses
    
    * chore: run Biome check:w
    
    UrlRepo.queryAlias is a helper function that queries by the original URL to find an existing alias with the purpose of preventing duplication. If an alias exists under that original URL, that alias is returned, otherwise the process continues on with creating the shortened URL. Before the fix, querying with this helper would return the entire object rather than just the alias. Now the deduplication functionality is properly working as intended.

--- packages/api/src/features/urls/url.repo.ts
+++ packages/api/src/features/urls/url.repo.ts
@@ -4,11 +4,15 @@ import type { Result } from "./url.service"
 export const UrlRepo = {
   async queryAlias(originalUrl: string): Promise<Result<string | null>> {
     try {
-      return [
-        (await UrlsModel.findOne({ original_url: originalUrl }, { alias: 1 }))
-          ?? null,
-        null,
-      ]
+      const query = await UrlsModel.findOne(
+        { original_url: originalUrl },
+        { alias: 1 },
+      )
+      if (query === null) {
+        return [null, null]
+      }
+
+      return [query.alias, null]
     } catch (err) {
       return [null, new Error(`${err}`)]
     }
@@ -17,7 +21,8 @@ export const UrlRepo = {
   async queryOriginalUrl(alias: string): Promise<Result<string | null>> {
     try {
       return [
-        (await UrlsModel.findOne({ alias }, { original_url: 1 })) ?? null,
+        (await UrlsModel.findOne({ alias }, { original_url: 1 }))?.alias
+          ?? null,
         null,
       ]
     } catch (err) {
