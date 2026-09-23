42487441136498bdea011bc3d25fa3306d0f3bc6
yuriongit <yuri.dev44@outlook.com>
2026-09-23 09:19:45 -0400

    refactor: rephrase footer's project description and remove redundant 'target' from redirection message

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export const Footer = () => (
   <footer className="w-full px-5 py-4 sm:px-8 text-center">
     <p className="text-sm text-stone-500 dark:text-stone-500 max-w-2xs px-5 mx-auto">
-      A project built to learn and practice GitHub Actions and Docker.
+      A URL shortener built to learn and apply Docker containerization and
+      GitHub Actions for CI/CD.
     </p>
   </footer>
 )
